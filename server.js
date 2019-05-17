535470a67041e2a58b943b393bf963c139059723
dominhthang <dominthang1999@gmail.com>
2019-05-17 15:27:23 +0700

    share with team

--- server.js
+++ server.js
@@ -3,6 +3,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+var pg = require('pg');
+var config = {
+    user: 'postgres',
+    database: 'clothes',
+    password: '1',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000,
+}
+var pool = new pg.Pool(config);
+
 let hbs = require('express-hbs');
 
 // Do Registration routes.
@@ -10,17 +23,11 @@ require('./routes')(app);
 
 //Set static content
 app.use('/static', express.static('./public'));
-
-// Set view template engine for file extension server.view.html
-app.engine('server.view.html', hbs.express4({
-  extname: '.server.view.html'
-}));
-
 //Set view engine
-app.set('view engine', 'server.view.html');
+app.set('view engine', 'ejs');
 
 //Set view folder
-app.set('views', './views/homepage');
+app.set('views', './views');
 
 app.listen(port, function(err){
   if(err){
@@ -28,4 +35,23 @@ app.listen(port, function(err){
     console.error(err);
   }
   console.log('App listen on port ' + port);
+});
+
+// --------- show san pham --------//
+app.get("/products/list", function(req, res){
+  pool.connect(function(err, client, done){
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT * FROM shop ORDER BY id_product ASC', function(err, result) {
+      done();
+
+      if(err) {
+        res.end();
+        return console.error('error running query', err);
+      }
+      // console.log(result.rows[0].ten_mat_hang);
+      res.render("products.list.ejs", {danhsach:result});
+    });
+  });
 });
