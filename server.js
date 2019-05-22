ba0c73fae2b207c86acc5c9067acbff28fa638bc
dominhthang <dominthang1999@gmail.com>
2019-05-22 19:39:45 +0700

    update insert product

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const pg = require('pg');
-
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -37,7 +38,7 @@ app.listen(port, function(err){
 });
 
 // --------- show san pham --------//
-app.get("/products/list", function(req, res){
+app.get("/admin/products/list", function(req, res){
   pool.connect(function(err, client, done){
     if(err) {
       return console.error('error fetching client from pool', err);
@@ -79,18 +80,18 @@ app.get("/admin/products/insert", (req, res) => {
   res.render("product.insert.ejs");
 })
 
-app.post("/admin/products/insert", (req, res) => {
+app.post("/admin/products/insert", urlencodedParser, (req, res) => {
   pool.connect((err, client, done) => {
     if(err) {
       console.log(err);
     }
 
-    var ten_mat_hang = req.body.txt_ten_mat_hang;
-    var loai_mat_hang = req.body.txt_loai_mat_hang;
-    var so_luong_san_pham = req.body.txt_so_luong_san_pham;
-    var gia_moi_san_pham = req.body.txt_gia_moi_san_pham;
-    var mo_ta_san_pham = req.body.txt_mo_ta_san_pham;
-    var hinh_anh_san_pham = req.body.txt_hinh_anh_san_pham;
+    let ten_mat_hang = req.body.txt_ten_mat_hang;
+    let loai_mat_hang = req.body.txt_loai_mat_hang;
+    let so_luong_san_pham = req.body.txt_so_luong_san_pham;
+    let gia_moi_san_pham = req.body.txt_gia_moi_san_pham;
+    let mo_ta_san_pham = req.body.txt_mo_ta_san_pham;
+    let hinh_anh_san_pham = req.body.txt_hinh_anh_san_pham;
 
     let query =`INSERT INTO shop(ten_mat_hang,
        loai_mat_hang,
@@ -103,7 +104,7 @@ app.post("/admin/products/insert", (req, res) => {
        ${so_luong_san_pham},
        ${gia_moi_san_pham},
        '${mo_ta_san_pham}',
-       '/img/${hinh_anh_san_pham}')`;
+       '/static/img/${hinh_anh_san_pham}')`;
 
     client.query(query, (err, result) => {
       done();
@@ -111,7 +112,16 @@ app.post("/admin/products/insert", (req, res) => {
         res.end();
         return console.error('error running query', err);
       }
-      res.redirect("../products/list");
+      res.redirect("/admin/products/list");
     })
   })
 })
+
+// // ------------- Edit Product ----------------
+//
+// app.get()
+//
+// app.post()
+//
+// // ------------- Remove Product --------------
+//
